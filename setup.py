7579ed29b975c493ebb859da4dbbe0c3142031e2
Anders Dahnielson <anders@dahnielson.com>
2007-10-24 14:13:55 +0000

    Release 0.4b
    git-svn-id: http://pyftgl.googlecode.com/svn/trunk@23 55cb297a-013d-0410-ae92-3bb49966ff91

--- setup.py
+++ setup.py
@@ -2,9 +2,18 @@
 
 from distutils.core import setup, Extension
 
+__version__ = "0.4b"
 __author__ = "Anders Dahnielson"
-__doc__ = "Pyhon FTGL bindings"
-__version__ = "0.3a"
+__doc__ = """Python FTGL binding
+
+FTGL is a free, open source library to enable developers to use
+arbitrary fonts in their OpenGL applications. Unlike other OpenGL font
+libraries FTGL uses standard font file formats and the Freetype font
+library to open the fonts. PyFTGL in turn wraps the functionality of
+FTGL into a Python module so that it can be used in conjunction with
+PyOpenGL."""
+
+doclines = __doc__.split("\n")
 
 ##########################################################################
 ## FTGL module
@@ -45,14 +54,19 @@ setup(
     maintainer = __author__,
     maintainer_email = 'anders@dahnielson.com',
     url = 'http://code.google.com/p/pyftgl/',
-    description = __doc__,
-    long_description = """FTGL is a free, open source library to enable developers
-    to use arbitrary fonts in their OpenGL applications. Unlike other OpenGL font
-    libraries FTGL uses standard font file formats and the Freetype font library to
-    open the fonts. PyFTGL in turn wraps the functionality of FTGL into a Python
-    module so that it can be used in conjunction with PyOpenGL.""",
-    download_url='http://code.google.com/p/pyftgl/downloads/list',
+    description = doclines[0],
+    long_description = "\n".join(doclines[2:]),
+    download_url = 'http://code.google.com/p/pyftgl/downloads/list',
+    classifiers = [
+    'Development Status :: 4 - Beta',
+    'Intended Audience :: Developers',
+    'License :: OSI Approved :: GNU General Public License (GPL)',
+    'Operating System :: POSIX',
+    'Programming Language :: C++',
+    'Programming Language :: Python',
+    'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
     license = "GPLv2",
-    platforms = "Linux",
+    platforms = "POSIX",
     ext_modules = [module_ftgl],
     )
